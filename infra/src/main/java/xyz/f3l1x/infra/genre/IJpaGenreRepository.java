6f25e736a651a655d1983ad63502afb05700dbaf
Felix Müller <felix.mueller0998@gmx.de>
2024-07-09 22:34:52 +0200

    Remove unnecessary repository annotation

--- infra/src/main/java/xyz/f3l1x/infra/genre/IJpaGenreRepository.java
+++ infra/src/main/java/xyz/f3l1x/infra/genre/IJpaGenreRepository.java
@@ -1,11 +1,9 @@
 package xyz.f3l1x.infra.genre;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface IJpaGenreRepository extends JpaRepository<GenreEntity, Long> {
     List<GenreEntity> findAllByIdIn(List<Long> ids);
 }
