87917806ec258fba5860c4d908829e54b3bd1134
Felix Müller <felix.mueller0998@gmx.de>
2024-07-12 23:19:10 +0200

    Add missing UpdateAudioBookRequest

--- api/src/main/java/xyz/f3l1x/open_audio_books_api/app/audio_book/dto/UpdateAudioBookRequest.java
+++ api/src/main/java/xyz/f3l1x/open_audio_books_api/app/audio_book/dto/UpdateAudioBookRequest.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CreateAudioBookRequest {
+public class UpdateAudioBookRequest {
     @JsonProperty()
     private String title;
     @JsonProperty()
