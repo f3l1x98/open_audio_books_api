cc8a3e80112bca6397e57e5f867cbc3106bae403
Felix Müller <felix.mueller0998@gmx.de>
2024-07-16 00:07:03 +0200

    Add controller tests for audio book delete

--- api/src/test/java/xyz/f3l1x/open_audio_books_api/app/audio_book/AudioBookControllerTests.java
+++ api/src/test/java/xyz/f3l1x/open_audio_books_api/app/audio_book/AudioBookControllerTests.java
@@ -1,8 +1,10 @@
 package xyz.f3l1x.open_audio_books_api.app.audio_book;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -15,7 +17,9 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import xyz.f3l1x.core.audio_book.AudioBook;
 import xyz.f3l1x.core.audio_book.command.create.CreateAudioBookCommand;
+import xyz.f3l1x.core.audio_book.command.delete.DeleteAudioBookCommand;
 import xyz.f3l1x.core.audio_book.command.update.UpdateAudioBookCommand;
+import xyz.f3l1x.core.audio_book.exception.AudioBookNotFoundException;
 import xyz.f3l1x.core.audio_book.query.find_all.FindAllAudioBookQuery;
 import xyz.f3l1x.core.episode.Episode;
 import xyz.f3l1x.core.shared.cqrs.ICommandHandler;
@@ -38,6 +42,8 @@ public class AudioBookControllerTests {
     private ICommandHandler<CreateAudioBookCommand, AudioBook> createAudioBookCommandHandler;
     @MockBean
     private ICommandHandler<UpdateAudioBookCommand, AudioBook> updateAudioBookCommandHandler;
+    @MockBean
+    private ICommandHandler<DeleteAudioBookCommand, AudioBook> deleteAudioBookCommandHandler;
 
     @Test
     public void testGetAllAudioBooks_success() throws Exception {
@@ -74,5 +80,42 @@ public class AudioBookControllerTests {
         mockMvc.perform(get("/api/v1/audio-book"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    public void testDeleteAudioBook_success() throws Exception {
+        AudioBook mockAudioBook = new AudioBook(
+                "Mock 1",
+                "Mock 1",
+                new Date(),
+                true,
+                0,
+                new ArrayList<>(),
+                new HashSet<>());
+
+        when(deleteAudioBookCommandHandler.handle(any(DeleteAudioBookCommand.class))).thenReturn(mockAudioBook);
+
+        mockMvc.perform(delete("/api/v1/audio-book/" + mockAudioBook.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(mockAudioBook.getId().toString())))
+                .andExpect(jsonPath("$.title", is(mockAudioBook.getTitle())));
+    }
+
+    @Test
+    public void testDeleteAudioBook_noContent() throws Exception {
+        AudioBook mockAudioBook = new AudioBook(
+                "Mock 1",
+                "Mock 1",
+                new Date(),
+                true,
+                0,
+                new ArrayList<>(),
+                new HashSet<>());
+
+        when(deleteAudioBookCommandHandler.handle(any(DeleteAudioBookCommand.class))).thenThrow(new AudioBookNotFoundException(mockAudioBook.getId()));
+
+        mockMvc.perform(delete("/api/v1/audio-book/" + mockAudioBook.getId()))
+                .andExpect(status().isNoContent())
+                .andExpect(jsonPath("$").doesNotExist());
     }
 }
