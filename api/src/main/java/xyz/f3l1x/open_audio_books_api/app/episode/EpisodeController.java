f39e16adb62da8c8bfbac4e44434cc13090659d2
Felix Müller <felix.mueller0998@gmx.de>
2024-08-19 18:07:13 +0200

    Add REST endpoint for streaming audio file

--- api/src/main/java/xyz/f3l1x/open_audio_books_api/app/episode/EpisodeController.java
+++ api/src/main/java/xyz/f3l1x/open_audio_books_api/app/episode/EpisodeController.java
@@ -1,9 +1,11 @@
 package xyz.f3l1x.open_audio_books_api.app.episode;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import xyz.f3l1x.core.audio_book.command.add_new_episode.AddNewEpisodeCommand;
 import xyz.f3l1x.core.audio_book.exception.AudioBookNotFoundException;
@@ -13,36 +15,44 @@ import xyz.f3l1x.core.episode.command.delete.DeleteEpisodeCommand;
 import xyz.f3l1x.core.episode.command.update.UpdateEpisodeCommand;
 import xyz.f3l1x.core.episode.exception.EpisodeNotFoundException;
 import xyz.f3l1x.core.episode.query.find_all_for_audio_book.FindAllForAudioBookEpisodeQuery;
+import xyz.f3l1x.core.episode.query.get_file.GetFileQuery;
+import xyz.f3l1x.core.episode.query.get_file.GetFileQueryResult;
 import xyz.f3l1x.core.shared.cqrs.ICommandHandler;
 import xyz.f3l1x.core.shared.cqrs.IQueryHandler;
 import xyz.f3l1x.open_audio_books_api.app.episode.dto.CreateEpisodeRequest;
 import xyz.f3l1x.open_audio_books_api.app.episode.dto.EpisodeDto;
 import xyz.f3l1x.open_audio_books_api.app.episode.dto.UpdateEpisodeRequest;
 
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 @RestController()
 @RequestMapping("/api/v1/audio-book/{audioBookId}/episode")
+@Slf4j
 public class EpisodeController {
     private final ModelMapper mapper;
     private final IQueryHandler<FindAllForAudioBookEpisodeQuery, List<Episode>> findAllForAudioBookEpisodeQueryHandler;
     private final ICommandHandler<DeleteEpisodeCommand, Episode> deleteEpisodeCommandHandler;
     private final ICommandHandler<UpdateEpisodeCommand, Episode> updateEpisodeCommandHandler;
     private final ICommandHandler<AddNewEpisodeCommand, Episode> addNewEpisodeCommandHandler;
+    private final IQueryHandler<GetFileQuery, GetFileQueryResult> getFileQueryHandler;
 
     public EpisodeController(
             ModelMapper mapper,
             IQueryHandler<FindAllForAudioBookEpisodeQuery, List<Episode>> findAllForAudioBookEpisodeQueryHandler,
             ICommandHandler<DeleteEpisodeCommand, Episode> deleteEpisodeCommandHandler,
             ICommandHandler<UpdateEpisodeCommand, Episode> updateEpisodeCommandHandler,
-            ICommandHandler<AddNewEpisodeCommand, Episode> addNewEpisodeCommandHandler
+            ICommandHandler<AddNewEpisodeCommand, Episode> addNewEpisodeCommandHandler,
+            IQueryHandler<GetFileQuery, GetFileQueryResult> getFileQueryHandler
     ) {
         this.mapper = mapper;
         this.findAllForAudioBookEpisodeQueryHandler = findAllForAudioBookEpisodeQueryHandler;
         this.deleteEpisodeCommandHandler = deleteEpisodeCommandHandler;
         this.updateEpisodeCommandHandler = updateEpisodeCommandHandler;
         this.addNewEpisodeCommandHandler = addNewEpisodeCommandHandler;
+        this.getFileQueryHandler = getFileQueryHandler;
     }
 
     @GetMapping("")
@@ -116,5 +126,24 @@ public class EpisodeController {
         }
 
         return ResponseEntity.ok(mapper.map(result, EpisodeDto.class));
+    }
+
+    @GetMapping("/{episodeId}/stream")
+    public ResponseEntity<Object> streamEpisode(@PathVariable("episodeId") UUID episodeId) {
+        try {
+            GetFileQuery query = new GetFileQuery(episodeId);
+            GetFileQueryResult result = getFileQueryHandler.handle(query);
+
+            InputStreamResource inputStreamResource = new InputStreamResource(result.stream());
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentLength(result.contentLength());
+            headers.set(HttpHeaders.CONTENT_TYPE, result.mimeType());
+            return ResponseEntity.ok().headers(headers).body(inputStreamResource);
+        } catch (EpisodeNotFoundException | FileNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            log.error(Arrays.deepToString(e.getStackTrace()));
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
