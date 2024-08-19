8260764c9498ed0ce482b02abe38844d5405afd1
Felix Müller <felix.mueller0998@gmx.de>
2024-08-19 17:57:15 +0200

    Add Bean for episode ContentStore and enable FilesystemStores

--- api/src/main/java/xyz/f3l1x/open_audio_books_api/app/config/InfraBeans.java
+++ api/src/main/java/xyz/f3l1x/open_audio_books_api/app/config/InfraBeans.java
@@ -2,18 +2,22 @@ package xyz.f3l1x.open_audio_books_api.app.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.content.fs.config.EnableFilesystemStores;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import xyz.f3l1x.core.audio_book.IAudioBookRepository;
 import xyz.f3l1x.core.author.IAuthorRepository;
 import xyz.f3l1x.core.episode.IEpisodeRepository;
+import xyz.f3l1x.core.episode.IEpisodeStore;
 import xyz.f3l1x.core.genre.IGenreRepository;
 import xyz.f3l1x.core.narrator.INarratorRepository;
 import xyz.f3l1x.infra.audio_book.IJpaAudioBookRepository;
 import xyz.f3l1x.infra.audio_book.JpaAudioBookRepository;
 import xyz.f3l1x.infra.author.IJpaAuthorRepository;
 import xyz.f3l1x.infra.author.JpaAuthorRepository;
+import xyz.f3l1x.infra.episode.EpisodeContentStore;
+import xyz.f3l1x.infra.episode.IEpisodeContentStore;
 import xyz.f3l1x.infra.episode.IJpaEpisodeRepository;
 import xyz.f3l1x.infra.episode.JpaEpisodeRepository;
 import xyz.f3l1x.infra.genre.IJpaGenreRepository;
@@ -23,6 +27,7 @@ import xyz.f3l1x.infra.narrator.JpaNarratorRepository;
 
 @Configuration()
 @EnableJpaRepositories(basePackages = "xyz.f3l1x.infra")
+@EnableFilesystemStores(basePackages = "xyz.f3l1x.infra")
 @EntityScan(basePackages = "xyz.f3l1x.infra")
 class InfraBeans {
 
@@ -49,5 +54,10 @@ class InfraBeans {
     @Bean
     public INarratorRepository narratorRepository(ModelMapper modelMapper, IJpaNarratorRepository jpaNarratorRepository) {
         return new JpaNarratorRepository(modelMapper, jpaNarratorRepository);
+    }
+
+    @Bean
+    public IEpisodeStore episodeStore(ModelMapper modelMapper, IEpisodeContentStore episodeContentStore) {
+        return new EpisodeContentStore(modelMapper, episodeContentStore);
     }
 }
